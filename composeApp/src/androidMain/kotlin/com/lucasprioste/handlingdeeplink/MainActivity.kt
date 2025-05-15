f669358570af088aa79f694b22205e54c4419714
LucasPrioste <lucasprioste@hotmail.com>
2025-05-15 11:10:13 +0100

    update code

--- composeApp/src/androidMain/kotlin/com/lucasprioste/handlingdeeplink/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/lucasprioste/handlingdeeplink/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.lucasprioste.handlingdeeplink
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.lucasprioste.handlingdeeplink.presentation.core.navigation.DeepLinkHelper
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+    private val deepLinkHelper: DeepLinkHelper by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             App()
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        intent.dataString?.let { deepLinkHelper.handle(it) }
     }
 }
 
